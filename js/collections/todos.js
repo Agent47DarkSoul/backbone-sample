2cace8171640a796d4cd94e852269ae393f01a42
Danish Satkut <danish_satkut@hotmail.com>
2013-07-25 21:22:01 +0530

    Added methods to get completed and remaining todos within a collection.

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -1,4 +1,22 @@
 var TodoCollection = Backbone.Collection.extend({
   model: Todo,
-  url: "todos.json"
+  url: "todos.json",
+  
+  completed: function () {
+    return this.filter(function (todo) {
+      return todo.get('completed');
+    })
+  },
+
+  remaining: function () {
+    return this.without.apply(this, this.completed());
+  },
+
+  nextId: function () {
+    if(!this.length) {
+      return 1;
+    }
+
+    return this.last().get('id') + 1;
+  }
 });
